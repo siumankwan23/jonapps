dd72d3c641ec58248354a00a7dca09b599f6afb7
siumankwan23 <76469093+siumankwan23@users.noreply.github.com>
2024-05-11 18:43:18 -0700

    Update Hello.py
    k

--- Hello.py
+++ Hello.py
@@ -14,7 +14,7 @@ st.sidebar.success("Select a tool above.")
 
 st.markdown(
     """
-    In the contemporary job market, resumes submitted for each job posting undergo initial pre-screening, scoring, and ranking by HR systems. These processes enable recruiters to swiftly identify the most qualified candidates to advance to the next stage in the hiring process. Consequently, it is crucial to craft your resume with the pertinent information to distinguish yourself. This website is designed to bridge the disparity between your resume and the job description, aiding you in standing out from the crowd by providing a tailored cover letter.
+ In the contemporary job market, resumes submitted for each job posting undergo initial pre-screening, scoring, and ranking by HR systems. These processes enable recruiters to swiftly identify the most qualified candidates to advance to the next stage in the hiring process. Consequently, it is crucial to craft your resume with the pertinent information to distinguish yourself. This website is designed to bridge the disparity between your resume and the job description, aiding you in standing out from the crowd by providing a tailored cover letter. After getting past the initial screening, use the interview helper to train for the interview.
     - Applying for a job(https://skjobapps.streamlit.app/Applying)
     - Interviewing for a job(https://skjobapps.streamlit.app/Interviewing)
     """
